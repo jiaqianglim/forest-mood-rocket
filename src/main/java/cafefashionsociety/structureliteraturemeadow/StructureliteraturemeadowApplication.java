cc8bb87c9e262cb7fe52b999ca1a70fb6b3763f0
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-08-15 19:14:49 +0800

    first commit

---
+++ src/main/java/cafefashionsociety/structureliteraturemeadow/StructureliteraturemeadowApplication.java
@@ -0,0 +1,13 @@
+package cafefashionsociety.structureliteraturemeadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureliteraturemeadowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructureliteraturemeadowApplication.class, args);
+	}
+
+}

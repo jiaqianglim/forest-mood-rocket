01221cd5836aaa99001ddbd26fbd154230aa6f10
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-08-21 21:00:21 +0800

    first commit

--- src/main/java/cafefashionsociety/structureliteraturemeadow/model/Profile.java
+++ src/main/java/cafefashionsociety/structureliteraturemeadow/model/Profile.java
@@ -1,42 +1,16 @@
 package cafefashionsociety.structureliteraturemeadow.model;
 
+import java.util.List;
+
 public class Profile {
+    private String id;
     private String displayName;
-    private String email;
+    private String organisationEmail;
     private String organisationName;
     private String organisationRole;
     private String pictureURL;
-    
-    public String getDisplayName() {
-        return displayName;
-    }
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getOrganisationName() {
-        return organisationName;
-    }
-    public void setOrganisationName(String organisationName) {
-        this.organisationName = organisationName;
-    }
-    public String getOrganisationRole() {
-        return organisationRole;
-    }
-    public void setOrganisationRole(String organisationRole) {
-        this.organisationRole = organisationRole;
-    }
-    public String getPictureURL() {
-        return pictureURL;
-    }
-    public void setPictureURL(String pictureURL) {
-        this.pictureURL = pictureURL;
-    }
-
+    private String userAccId;
+    private List<Report> createdReports;
+    private List<Report> sharedReports;
     
 }
--- src/main/java/cafefashionsociety/structureliteraturemeadow/model/Report.java
+++ src/main/java/cafefashionsociety/structureliteraturemeadow/model/Report.java
@@ -1,5 +1,24 @@
 package cafefashionsociety.structureliteraturemeadow.model;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import com.redis.om.spring.annotations.Document;
+import com.redis.om.spring.annotations.Indexed;
+
+@Document
 public class Report {
     // Incident, Task, Reflection, Workshop
+    @Id
+    @Indexed
+    private String id;
+    private String userId;
+    private String profileId;
+    private Date creationDate;
+    private Date incidentnDate;
+    private String title;
+    private String what;
+    private String soWhat;
+    private String tags;
 }
